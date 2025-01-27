326432790ec00307d505575ae5b85293a4aac441
kematian05 <orucov.vuset@gmail.com>
2025-01-27 17:19:19 +0400

    Monday work

--- app/src/main/java/com/example/testing/MainActivity.kt
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -9,11 +9,16 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.ScrollableDefaults.flingBehavior
+import androidx.compose.foundation.gestures.snapping.SnapLayoutInfoProvider
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -25,6 +30,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -45,6 +51,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -54,6 +62,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
@@ -136,6 +145,16 @@ val postArr: List<Post> = listOf(
         likes = 100000,
         comments = 10000,
         shares = 5000,
+        caption = "Just posted a new video on my YouTube channel! Check it out!"
+    ),
+    Post(
+        profile = userArr[3],
+        postImages = arrayListOf(
+            R.drawable.can_yaman
+        ),
+        likes = 1000000,
+        comments = 100000,
+        shares = 50000,
         caption = "Just posted a new video on my YouTube channel! Check it out!"
     )
 )
@@ -250,6 +269,7 @@ fun StoryList(
     }
 }
 
+@SuppressLint("RememberReturnType", "FrequentlyChangedStateReadInComposition")
 @Composable
 fun InstagramPostCard(
     post: Post,
@@ -299,11 +319,25 @@ fun InstagramPostCard(
                 )
             }
         }
-        val listState = rememberLazyListState()
-        Box{
+
+        val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+        val lazyRowState = rememberLazyListState()
+        Column(
+            modifier = Modifier
+        ) {
+            Text(
+                text = "Post ${postArr.indexOf(post) + 1}",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    color = Color.Black
+                ),
+                modifier = Modifier
+                    .padding(start = 5.dp)
+                    .
+            )
             LazyRow(
-                state = listState,
-                modifier = Modifier.fillMaxWidth(),
+                state = lazyRowState,
+                flingBehavior = rememberSnapFlingBehavior(lazyRowState)
             ) {
                 items(post.postImages.size) { index ->
                     Image(
@@ -312,39 +346,7 @@ fun InstagramPostCard(
                         contentScale = ContentScale.Crop,
                         modifier = Modifier
                             .height(450.dp)
-                            .width(300.dp)
-                    )
-                    if (post.postImages.size > 1) {
-                        Text(
-                            text = "${index + 1}/${post.postImages.size}",
-                            style = TextStyle(color = Color.White, fontSize = 14.sp),
-                            modifier = Modifier
-                                .align(Alignment.TopEnd)
-                                .padding(8.dp)
-                                .background(
-                                    color = Color.Black.copy(alpha = 0.7f),
-                                    shape = RoundedCornerShape(12.dp)
-                                )
-                                .padding(horizontal = 8.dp, vertical = 4.dp)
-                        )
-                    }
-
-                }
-            }
-            Row(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(8.dp),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                post.postImages.forEachIndexed { index, _ ->
-                    val isSelected = listState.firstVisibleItemIndex == index
-                    Box(
-                        modifier = Modifier
-                            .padding(4.dp)
-                            .size(if (isSelected) 10.dp else 8.dp)
-                            .clip(CircleShape)
-                            .background(if (isSelected) Color.Black else Color.Gray)
+                            .width(screenWidth)
                     )
                 }
             }
